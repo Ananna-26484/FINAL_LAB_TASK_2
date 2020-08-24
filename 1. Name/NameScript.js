e8c32c9bbdb0ce64cde92eafe13aa30abbab04cb
Sanjida Akter Ananna <14-26484-2@student.aiub.edu>
2020-08-24 22:19:17 +0600

    script updated

--- 1. Name/NameScript.js
+++ 1. Name/NameScript.js
@@ -7,11 +7,8 @@ function validate(){
 	if(username == ""){
 		document.getElementById('userMsg').innerHTML = "username can't left empty";
 		return false;
-	}
-	else if (username.length<3) {
-		document.getElementById('userMsg').innerHTML = "Name must be greater than 2 words";
-		return false;
-	}else{
+	}	
+	else{
 		return true;
 	}
 
